63d77cb1aec4c40f774a90afa98f7728c02592a8
arha83 <arha113918@gmail.com>
2021-12-27 15:17:18 +0330

    using mobilenetv2 model

--- main.py
+++ main.py
@@ -21,11 +21,22 @@ testImages, testlabels= read_hoda_dataset(dataset_path='./HodaDatasetReader/Digi
 remainImages, remainLabels= read_hoda_dataset(dataset_path='./HodaDatasetReader/DigitDB/RemainingSamples.cdb', images_height=32, images_width=32, one_hot=True, reshape=True)
 
 # normalizing dataset:
-trainImages= trainImages.reshape(trainImages.shape[0], 32, 32, 1) / 255.0
-testImages= testImages.reshape(testImages.shape[0], 32, 32, 1) / 255.0
-remainImages= remainImages.reshape(remainImages.shape[0], 32, 32, 1) / 255.0
+trainImages= trainImages.reshape(trainImages.shape[0], 32, 32, 1)
+testImages= testImages.reshape(testImages.shape[0], 32, 32, 1)
+remainImages= remainImages.reshape(remainImages.shape[0], 32, 32, 1)
+trainImages= cv.cvtColor(trainImages, cv.COLOR_GRAY2RGB) / 255.0
+testImages= cv.cvtColor(testImages, cv.COLOR_GRAY2RGB) / 255.0
+remainImages= cv.cvtColor(remainImages, cv.COLOR_GRAY2RGB) / 255.0
 
+# building the model:
+print('### building the model...')
+baseModel= applications.MobileNetV2(input_shape=(32,32,3), include_top=False, weights='imagenet')
+baseModel.trainable= False
+averageLayer= layers.GlobalAveragePooling2D()
+predictionLayer= layers.Dense(10)
+model= models.Sequential([baseModel, averageLayer, predictionLayer])
 
+print(model.summary())
 
 
 
